207ec36add495bc20ba52f3f616e8a4f5ed75701
kayra <mgulkajyr@inbox.ru>
2022-12-05 21:51:59 +0600

    offffff

---
+++ 3rdlesson2mHomeW/3rdlesson2mHomeW/main.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+//var check = Teachers(name: "name", surname: "surname", age:3 )
+//var check2 = Students(name: "name", surname: "surname", age: 12, ball: 2)
+//
+//check.addTeachers(teacher: Teachers(name: "Ruslan",   surname: "Sharshenov", age: 23))
+//check.addTeachers(teacher: Teachers(name: "Aisuluu",  surname: "Tinibekovna", age: 29))
+//check.addTeachers(teacher: Teachers(name: "Toktobyby",surname: "Kasimovna", age: 33))
+//check.addTeachers(teacher: Teachers(name: "Gilayim",  surname: "Sabirbekovna", age: 28))
+//check.addTeachers(teacher: Teachers(name: "Mira",     surname: "Begalievna", age: 42))
+//
+//check2.addStudents(student: Students(name: "Ali",    surname: "Azimov", age: 11, ball: 5))
+//check2.addStudents(student: Students(name: "Ilyaz",  surname: "Abakirov", age: 12, ball: 4))
+//check2.addStudents(student: Students(name: "Dima",   surname: "Ivanov", age: 10, ball: 3))
+//check2.addStudents(student: Students(name: "Ivan",   surname: "Ivanov", age: 8, ball: 5))
+//check2.addStudents(student: Students(name: "Dana",   surname: "Bekesov", age: 11, ball: 4))
+//check2.addStudents(student: Students(name: "Simbat", surname: "Asanova", age: 12, ball: 5))
+//check2.addStudents(student: Students(name: "Alena",  surname: "Aleksandrovna", age: 12, ball: 5))
+//check2.addStudents(student: Students(name: "Argen",  surname: "Taalaibekov", age: 11, ball: 4))
+//check2.addStudents(student: Students(name: "Deniz",  surname: "Maratova", age: 10, ball: 3))
+//check2.addStudents(student: Students(name: "Vera",   surname: "Nikolaevna", age: 11, ball: 2))
+//print("students")
+//
+//check2.countStudents()
+//check.countTeachers()
+//check.printInfo   (name: "Mira", surname: "Begalievna", age: 42)
+//check2.printInfo2 (name: "Ali"  ,surname: "Azimov",     age: 11, ball: 5)
+//check.removeTeachers(teacher: 2)
+
+
+
+// var ache = Hospital()
+//ache.addPatientToArray(patient: Ailment(name: "Aziz", surname: "Aliev", sick: "gastritis"))
+//ache.addPatientToArray(patient: Ailment(name: "Zara", surname: "Nikitina", sick: "Cold"))
+//ache.printInfo(name: "Zara")
+//                            
+//                           
+//ache.printInfo(name: "Aziz")
+//ache.printInfo(name: "keraf")
+
+
+//#1. Создать класс DataBase.
+//
+//Внутри создать  2 массива типа Students и Teachers(создать эти 2 класса(модели), потом использовать)
+//
+//Для каждого из классов, касающихся базы данных должны быть методы для добавления, удаления и считывания учеников/учителей.
+//Другими способами ничего изменяться не должно
+//Так же добавить методы, позволяющие увидеть полный список(String) учеников и учителей.
+//Пример:
+//#1 - Имя Фамилия - Возраст - Класс - Средний балл
+//
+//№2.
+//Создать класс Больница
+//Создать класс пациент
+//Добавить массив пациентов в больницу
+//Создать приватную функцию поиска по фамилии и имени, добавление(с учетом что такого человека еще нет) и удаление с учетом если есть
+//В main создать объект Больницы и протестировать ваш код разными кейсами
+
+
